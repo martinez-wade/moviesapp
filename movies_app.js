e70cfa642334d13ae0de5c02f331151b8804a85e
sam's macbook <samuel2.martinez2@gmail.com>
2022-06-07 09:45:25 -0500

    canged song-movie

--- movies_app.js
+++ movies_app.js
@@ -12,11 +12,11 @@ const movies = () => {
 }
 movies();
 
-const renderSongHTML = () => {
-    console.log("Rendering song HTML")
+const renderMovieHTML = () => {
+    console.log("Rendering movies HTML")
     movies().then((data) => {
         console.log(data);
-        let songCards = data.map(movie => {
+        let MovieCards = data.map(movie => {
             return `
             <div>
             <h3>Title: ${movie.title}</h3>
@@ -27,11 +27,11 @@ const renderSongHTML = () => {
             </div>
             `
         })
-        console.log(songCards);
-        document.getElementById("library").innerHTML = songCards.join("");
+        console.log(MovieCards);
+        document.getElementById("library").innerHTML = MovieCards.join("");
     })
 }
-renderSongHTML();
+renderMovieHTML();
 
 // // FETCH A SONG BY ID
 //
@@ -75,20 +75,20 @@ renderSongHTML();
 
 // PATCH METHOD
 
-// const editSong = (song) => {
-//     const URL = "https://acoustic-little-chive.glitch.me/songs";
+// const editMovie = (movie) => {
+//     const URL = "https://lean-imported-ballcap.glitch.me/movies";
 //     let options = {
 //         method: "PATCH",
 //         headers: {
 //             // Content-Type : tells the server what type of data we are sending with our request. When interacting with a JSON API, this will usually be in application/json.
 //             'Content-Type': 'application/json' // establishing the format in which we send the data.
 //         },
-//         body: JSON.stringify(song) // convert the JS object into a JSON String before sending it to the server.
+//         body: JSON.stringify(movie) // convert the JS object into a JSON String before sending it to the server.
 //     }
 //
-//     return fetch(`${URL}/${song.id}`, options).then(resp => resp.json())
+//     return fetch(`${URL}/${movie.id}`, options).then(resp => resp.json())
 // }
-//
+
 // let editedSong = {
 //     id: 1,
 //     title: "TNT",
@@ -98,31 +98,44 @@ renderSongHTML();
 
 // POST REQUEST
 
-// const addSong = (songObj) => {
-//     const URL = "https://acoustic-little-chive.glitch.me/songs";
-//     let options = {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(songObj)
-//     }
-//     return fetch(URL, options).then(res => res.json()).then(result => console.log("You've successfully created a new song!", result))
-// }
-//
-//
-// // Whenever the "Add Song" button is clicked, create new object based on the input fields.
-// document.getElementById("addSong").addEventListener("click", function (e) {
-//     e.preventDefault();
-//     let newSong = {
-//         title: document.getElementById("title").value,
-//         artist: document.getElementById("artist").value
-//     }
-//     addSong(newSong).then((res) => {
-//         console.log(res)
-//         renderSongHTML()
-//     })
-// })
+const addMovie = (movieObj) => {
+    const URL = "https://lean-imported-ballcap.glitch.me/movies";
+    let options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movieObj)
+    }
+    return fetch(URL, options).then(res => res.json()).then(result => console.log("You've successfully created a new movie!", result))
+}
+
+
+// Whenever the "Add Song" button is clicked, create new object based on the input fields.
+document.getElementById("addMovie").addEventListener("click", function (e) {
+    e.preventDefault();
+    let newMovie = {
+        title: document.getElementById("title").value,
+        artist: document.getElementById("artist").value
+    }
+    addMovie(newMovie).then((res) => {
+        console.log(res)
+        renderMovieHTML()
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 // // DELETE METHOD
 //
