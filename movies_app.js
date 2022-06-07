ef6458d79e159ad6889561a11eab8262668b9f6d
Kelvin <kelvin.daquon.wade@gmail.com>
2022-06-07 11:56:24 -0500

    update

--- movies_app.js
+++ movies_app.js
@@ -22,78 +22,44 @@ const renderMovieHTML = () => {
             <h3>Title: ${movie.title}</h3>
             <p>Artist: ${movie.director}</p>
             <p>Rating: ${movie.rating}</p>
-            <button data-id="${movie.id}">Edit</button>
-            <button data-id="${movie.id}">Delete</button>
+            <button data-id="${movie.id}" id="edit">Edit</button>
+            <button data-id="${movie.id}"id="delete">Delete</button>
             </div>
             `
         })
         console.log(MovieCards);
         document.getElementById("library").innerHTML = MovieCards.join("");
-    })
+    }).then(() => {
+        $("#edit").click(function(){
+            $(this).append("<form> </form>")
+        })
+
+        })
+
 }
 renderMovieHTML();
 
-// // FETCH A SONG BY ID
-//
-// const getSongById = (id) => {
-//     const URL = `https://acoustic-little-chive.glitch.me/songs/${id}`;
-//     return fetch(URL).then(res => res.json()).then(res => console.log(res));
-// }
-//
-// getSongById(8);
 
+// Editing button functionaility
+const editMovie = (movie) => {
+    const URL = "https://lean-imported-ballcap.glitch.me/movies";
+    let options = {
+        method: "PATCH",
+        headers: {
+            // Content-Type : tells the server what type of data we are sending with our request. When interacting with a JSON API, this will usually be in application/json.
+            'Content-Type': 'application/json' // establishing the format in which we send the data.
+        },
+        body: JSON.stringify(movie) // convert the JS object into a JSON String before sending it to the server.
+    }
 
-// EDIT (PUT vs PATCH)
+    return fetch(`${URL}/${movie.id}`, options).then(resp => resp.json().then())
+}
 
-// PUT method - requires you to resubmit the entire entity.
-// PATCH method - only edits properties that currently exist in the entity.
+let editedMovie = {
+    tittle: 1,
+    title: "TNT",
+}
 
-
-// PUT METHOD
-
-// const editSong = (song) => {
-//     const URL = "https://acoustic-little-chive.glitch.me/songs";
-//     let options = {
-//         method: "PUT",
-//         headers: {
-//             // Content-Type : tells the server what type of data we are sending with our request. When interacting with a JSON API, this will usually be in application/json.
-//             'Content-Type': 'application/json' // establishing the format in which we send the data.
-//         },
-//         body: JSON.stringify(song) // convert the JS object into a JSON String before sending it to the server.
-//     }
-//
-//     return fetch(`${URL}/${song.id}`, options).then(resp => resp.json())
-// }
-//
-// let editedSong = {
-//     id: 1,
-//     title: "Dirty Deeds",
-//     artist: "ACDC"
-// }
-//
-// editSong(editedSong);
-
-// PATCH METHOD
-
-// const editMovie = (movie) => {
-//     const URL = "https://lean-imported-ballcap.glitch.me/movies";
-//     let options = {
-//         method: "PATCH",
-//         headers: {
-//             // Content-Type : tells the server what type of data we are sending with our request. When interacting with a JSON API, this will usually be in application/json.
-//             'Content-Type': 'application/json' // establishing the format in which we send the data.
-//         },
-//         body: JSON.stringify(movie) // convert the JS object into a JSON String before sending it to the server.
-//     }
-//
-//     return fetch(`${URL}/${movie.id}`, options).then(resp => resp.json())
-// }
-
-// let editedSong = {
-//     id: 1,
-//     title: "TNT",
-// }
-//
 // editSong(editedSong);
 
 // POST REQUEST
@@ -116,7 +82,9 @@ document.getElementById("addMovie").addEventListener("click", function (e) {
     e.preventDefault();
     let newMovie = {
         title: document.getElementById("title").value,
-        artist: document.getElementById("artist").value
+        director: document.getElementById("director").value,
+        rating: document.getElementById("rating").value
+
     }
     addMovie(newMovie).then((res) => {
         console.log(res)
